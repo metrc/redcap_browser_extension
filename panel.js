6af004bf6de5b8335682fc26d52fb40e6c2c72fe
Paige Julianne Sullivan <paige@paigejulianne.com>
2023-08-29 20:54:52 -0400

    fixes to panel, including new features

--- panel.js
+++ panel.js
@@ -5,6 +5,11 @@ async function runMain() {
     let config_url = await chrome.storage.sync.get('redcap_path');
     config_url = config_url.redcap_path;
 
+    if ((config_url == undefined) || (config_url == '')) {
+        chrome.runtime.openOptionsPage();
+        window.close();
+    }
+
     // fetch the configuration file
     let config = await fetch(config_url);
     config = await config.json();
@@ -14,48 +19,27 @@ async function runMain() {
     $('#projects').autocomplete({ source: config.projects_url });
 
     if (config.super_user == false) {
-        $('#submit2').hide();
-        $('#submit3').hide();
-        $('#addUser').hide();
-        $('#manageEMs').hide();
+        $('#adminLinks').hide();
     }
-
-
-
-/*
-    chrome.storage.sync.get('project_json_path', function(data) {
-
-        let project_json = $.getJSON(data.project_json_path, function(data) {
-            let project_list = [];
-            for (let i = 0; i < data.length; i++) {
-                project_list.push({"value": data[i].project_id, "label": data[i].app_title});
-            }
-
-            $( "#projects" ).autocomplete({
-                source: project_list
-            });
-        });
-    });
-*/
 
 };
 
 document.addEventListener('DOMContentLoaded', runMain);
 
-document.getElementById('submit').addEventListener('click', () => {
+document.getElementById('goToRecord').addEventListener('click', () => {
     let project_id = $( "#projects" ).val();
     let record_id = $( "#record" ).val();
     let url = base_url + 'DataEntry/record_home.php?pid=' + project_id + '&id=' + record_id;
     chrome.tabs.create({url: url});
 });
 
-document.getElementById('submit2').addEventListener('click', () => {
+document.getElementById('goToUserAdmin').addEventListener('click', () => {
     let project_id = $( "#projects" ).val();
     let url = base_url + 'UserRights/index.php?pid=' + project_id;
     chrome.tabs.create({url: url});
 });
 
-document.getElementById('submit3').addEventListener('click', () => {
+document.getElementById('goToSetup').addEventListener('click', () => {
     let project_id = $( "#projects" ).val();
     let url = base_url + 'ProjectSetup/index.php?pid=' + project_id;
     chrome.tabs.create({url: url});
@@ -68,5 +52,22 @@ document.getElementById('addUser').addEventListener('click', () => {
 
 document.getElementById('manageEMs').addEventListener('click', () => {
     let url = base_url + 'ExternalModules/manager/control_center.php';
+    chrome.tabs.create({url: url});
+});
+
+document.getElementById('goToDesign').addEventListener('click', () => {
+    let project_id = $( "#projects" ).val();
+    let url = base_url + 'Design/online_designer.php?pid=' + project_id;
+    chrome.tabs.create({url: url});
+});
+
+document.getElementById('goToCodebook').addEventListener('click', () => {
+    let project_id = $( "#projects" ).val();
+    let url = base_url + 'Design/data_dictionary_codebook.php?pid=' + project_id;
+    chrome.tabs.create({url: url});
+});
+
+document.getElementById('searchUsers').addEventListener('click', () => {
+    let url = base_url + 'ControlCenter/view_users.php';
     chrome.tabs.create({url: url});
 });
