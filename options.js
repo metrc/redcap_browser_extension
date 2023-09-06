5bdd30dcd68165db41ccdd3318fc0582d2133258
Paige Julianne Sullivan <psullivan@jhu.edu>
2023-09-06 09:26:35 -0400

    support for multiple profiles

--- options.js
+++ options.js
@@ -1,34 +1,68 @@
+let profile_keys = [];
+let profile_names = [];
 
-function saveOptions() {
+async function saveOptions() {
     var configKey = document.getElementById('config_key').value;
-    chrome.storage.sync.set({'config_key': configKey}, function() {
-        window.close();
-    });
+    await chrome.storage.sync.set({'config_key': configKey});
+    saveProfiles();
+    window.close();
 }
 
-function restoreOptions() {
+async function restoreOptions() {
     chrome.storage.sync.get('config_key', function(data) {
-        if (data.config_key != undefined) {
-            document.getElementById('config_key').value = data.config_key;
+        document.getElementById('config_key').value = data.config_key;
+    });
+    chrome.storage.sync.get('multi_profile', function(data) {
+        document.getElementById("multi_profile").checked = data.multi_profile;
+        if (data.multi_profile == true) {
+            showMultiProfile();
+            restoreProfiles();
         }
     });
-
 }
 
-function showMultiProfile() {
+
+async function saveProfiles() {
+    for (var i = 0; i < 5; i++) {
+        profile_keys[i] = document.getElementById('profile_key_' + i).value;
+        profile_names[i] = document.getElementById('profile_name_' + i).value;
+    }
+    await chrome.storage.sync.set({'profile_keys': profile_keys});
+    await chrome.storage.sync.set({'profile_names': profile_names});
+}
+
+async function restoreProfiles() {
+    profile_keys = await chrome.storage.sync.get('profile_keys');
+    profile_names = await chrome.storage.sync.get('profile_names');
+    for (var i = 0; i < 5; i++) {
+        var profileKey = document.getElementById('profile_key_' + i).value = profile_keys.profile_keys[i];
+        var profileName = document.getElementById('profile_name_' + i).value = profile_names.profile_names[i];
+    }
+}
+
+async function showMultiProfile() {
     var multiProfile = document.getElementById("multi_profile");
     var multiProfileText = document.getElementById("multi_profile_text");
+    var singleProfileText = document.getElementById("single_profile_text");
+    var profileOne = document.getElementById("profile_key_0");
+    var singleProfile = document.getElementById("config_key");
     var body = document.getElementsByTagName("body")[0];
     if (multiProfile.checked == true){
         multiProfileText.style.display = "block";
         body.style.height = "500px";
+        singleProfileText.style.display = "none";
+        profileOne.value = singleProfile.value;
+        await chrome.storage.sync.set({'multi_profile': true});
     } else {
         multiProfileText.style.display = "none";
         body.style.height = "150px";
+        singleProfileText.style.display = "block";
+        singleProfile.value = profileOne.value;
+        await chrome.storage.sync.set({'multi_profile': false});
     }
 }
 
 
 document.getElementById('save').addEventListener('click', saveOptions);
-// document.getElementById('multi_profile').addEventListener('click', showMultiProfile);
+document.getElementById('multi_profile').addEventListener('click', showMultiProfile);
 document.addEventListener('DOMContentLoaded', restoreOptions);
--- panel.js
+++ panel.js
@@ -1,32 +1,85 @@
 base_url = '';
+multiProfile = false;
+profile_keys = [];
+profile_names = [];
 
 async function runMain() {
-    // fetch the config url from storage
-    let config_key = await chrome.storage.sync.get('config_key');
+    multiProfile = await chrome.storage.sync.get('multi_profile');
 
-    if ((config_key.config_key == undefined) || (config_key.config_key == '')) {
-        chrome.runtime.openOptionsPage();
-        window.close();
-        return;
+    if (!multiProfile.multi_profile) {
+        let config_key = await chrome.storage.sync.get('config_key');
+
+
+        if ((config_key.config_key === undefined) || (config_key.config_key == '')) {
+            chrome.runtime.openOptionsPage();
+            window.close();
+            return;
+        }
+        document.getElementById("profileoptions").style.display = "none";
+
+        config_data = config_key.config_key.toString().split('|', 6);
+
+        base_url = config_data[0] + 'redcap_v' + config_data[1] + '/';
+        projects_url = base_url + 'ExternalModules/?prefix='+config_data[2]+'&page=projects&pid='+config_data[3]+
+            '&NOAUTH&api_token='+config_data[4];
+
+        $('#projects').autocomplete({ source: projects_url });
+
+        if (config_data[5] == 0) {
+            $('#adminLinks').hide();
+        }
+
+    } else {
+        profile_keys = await chrome.storage.sync.get('profile_keys');
+        profile_names = await chrome.storage.sync.get('profile_names');
+
+        if ((profile_keys.profile_keys[0] === undefined) || (profile_keys.profile_keys[0] == '')) {
+            chrome.runtime.openOptionsPage();
+            window.close();
+            return;
+        }
+        document.getElementById("profileoptions").style.display = "block";
+        // populate the dropdown
+        let profile_dropdown = document.getElementById("profile");
+        var options = [];
+        for (var i = 0; i < 5; i++) {
+            options[i] = { value: i+1, label: profile_names.profile_names[i] };
+        }
+        $('#profile').autocomplete({ source: options });
     }
+};
 
-    config_data = config_key.config_key.toString().split('|', 6);
+async function profileOnChange() {
+    profile = $('#profile').val();
+    profile = profile - 1;
 
+    config_data = profile_keys.profile_keys[profile].split('|', 6);
     base_url = config_data[0] + 'redcap_v' + config_data[1] + '/';
+    console.log(base_url);
     projects_url = base_url + 'ExternalModules/?prefix='+config_data[2]+'&page=projects&pid='+config_data[3]+
         '&NOAUTH&api_token='+config_data[4];
 
     $('#projects').autocomplete({ source: projects_url });
-
     if (config_data[5] == 0) {
         $('#adminLinks').hide();
     }
+}
 
-};
+function checkForDefaultProfile() {
+    if (multiProfile.multi_profile) {
+        if ($('#profile').val() == '') {
+            $('#profile').val('1');
+            profileOnChange();
+        }
+    }
+}
 
 document.addEventListener('DOMContentLoaded', runMain);
+document.getElementById('profile').addEventListener('change', profileOnChange);
+document.getElementById('projects').addEventListener('click', checkForDefaultProfile);
 
 document.getElementById('goToRecord').addEventListener('click', () => {
+    checkForDefaultProfile()
     let project_id = $( "#projects" ).val();
     let record_id = $( "#record" ).val();
     let url = base_url + 'DataEntry/record_home.php?pid=' + project_id + '&id=' + record_id;
@@ -34,40 +87,47 @@ document.getElementById('goToRecord').addEventListener('click', () => {
 });
 
 document.getElementById('goToUserAdmin').addEventListener('click', () => {
+    checkForDefaultProfile()
     let project_id = $( "#projects" ).val();
     let url = base_url + 'UserRights/index.php?pid=' + project_id;
     chrome.tabs.create({url: url});
 });
 
 document.getElementById('goToSetup').addEventListener('click', () => {
+    checkForDefaultProfile()
     let project_id = $( "#projects" ).val();
     let url = base_url + 'ProjectSetup/index.php?pid=' + project_id;
     chrome.tabs.create({url: url});
 });
 
 document.getElementById('addUser').addEventListener('click', () => {
+    checkForDefaultProfile()
     let url = base_url + 'ControlCenter/create_user.php';
     chrome.tabs.create({url: url});
 });
 
 document.getElementById('manageEMs').addEventListener('click', () => {
+    checkForDefaultProfile()
     let url = base_url + 'ExternalModules/manager/control_center.php';
     chrome.tabs.create({url: url});
 });
 
 document.getElementById('goToDesign').addEventListener('click', () => {
+    checkForDefaultProfile()
     let project_id = $( "#projects" ).val();
     let url = base_url + 'Design/online_designer.php?pid=' + project_id;
     chrome.tabs.create({url: url});
 });
 
 document.getElementById('goToCodebook').addEventListener('click', () => {
+    checkForDefaultProfile()
     let project_id = $( "#projects" ).val();
     let url = base_url + 'Design/data_dictionary_codebook.php?pid=' + project_id;
     chrome.tabs.create({url: url});
 });
 
 document.getElementById('searchUsers').addEventListener('click', () => {
+    checkForDefaultProfile()
     let url = base_url + 'ControlCenter/view_users.php';
     chrome.tabs.create({url: url});
 });
